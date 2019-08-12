698e258a6acfdd4bfda33e19c9b3c422a5358d47
jymmak <jkmogollon.cp@gmail.com>
2019-08-12 11:18:36 -0400

    practice react

--- app/App.js
+++ app/App.js
@@ -5,7 +5,12 @@ import { render } from 'react-dom'
 class App extends React.Component {
     render() {
         return (
+            <React.Fragment>
             <h1>Curso React</h1>
+            <img  src="https://placeimg.com/200/400" alt=""/>
+            <br></br>
+            <button onClick={()=>alert('holaaa')}>Hello!!</button>
+            </React.Fragment>
         )
     }
 }
