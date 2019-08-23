3ef8a034d25f9e10d30b055542ef8e04a16094f7
jymmak <jkmogollon.cp@gmail.com>
2019-08-23 17:40:02 -0400

    state in component

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     entry: [
-        __dirname + "/app/Component.js",
+        __dirname + "/app/StateComponent.js",
     ],
     output: {
         path: __dirname + "/public",
